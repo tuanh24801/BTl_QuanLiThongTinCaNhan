88b961cdf0bf64f8d5b09673f03934b4c7a5fd34
phuong32123 <www.phuong932001vn@gmail.com>
2021-10-24 23:16:06 +0700

    xu ly dang ki

---
+++ src/xulydangki.php
@@ -0,0 +1,50 @@
+<?php
+    if(isset($_POST['btn_dangki'])){
+        $tentaikhoan = $_POST['tentaikhoan'];
+        $email = $_POST['email'];
+        $matkhau = $_POST['matkhau'];
+        $matkhau_2 = $_POST['matkhau_2'];
+        $checkbox_login = $_POST['checkbox_login'];
+        include './sendEmail/sendemail.php';
+        include './config/config.php';
+        $sql = "SELECT * FROM tb_taikhoan WHERE tentaikhoan = '$tentaikhoan'";
+            $result = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result) > 0){
+            header('Location: dangki.php?error=Tên tài khoản đã tồn tại');
+            exit();
+        }
+        if($checkbox_login != '1'){
+            header('Location: dangki.php?error=vui lòng đồng ý điều khoản sử dụng');
+            exit();
+        }
+        if($matkhau_2 != $matkhau){
+            header('Location: dangki.php?error=sai mật khẩu xác nhận');
+            exit();
+        }else{
+            // $sql = "SELECT * FROM tb_taikhoan WHERE tentaikhoan = '$tentaikhoan'";
+            // $result = mysqli_query($conn,$sql);
+            // if(mysqli_num_rows($result) > 0){
+            //     header('Location: dangki.php?error=Tên tài khoản đã tồn tại');
+            //     exit();
+            // }
+            $sql_1 = "SELECT * FROM tb_taikhoan WHERE email = '$email'";
+            $result_1 = mysqli_query($conn,$sql_1);
+            if(mysqli_num_rows($result_1) > 0){
+                header('Location: dangki.php?error=Gmail đã tồn tại');
+                exit();
+            }else{
+                $sql2 = "INSERT INTO tb_taikhoan(tentaikhoan, email, matkhau) VALUES ('$tentaikhoan', '$email', '$matkhau_2')";
+                $result2 = mysqli_query($conn,$sql2);
+                if($result2 > 0){
+                    sendEmail('www.phuong932001vn@gmail.com','code nè');
+                    header('Location: dangki.php?error=Vui lòng kiểm tra email để xác minh đăng kí');
+                    exit();
+                }else{
+                    header('Location: error/error.php');
+
+                }
+            }
+        }
+    }
+   
+?>

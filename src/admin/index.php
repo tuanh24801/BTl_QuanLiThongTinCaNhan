7b4a1ebc4c395b7f6ecf69a1bdce4057e7672c01
dao98765 <daont1810@gmail.com>
2021-10-27 20:10:49 +0700

    giao dien admin

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,14 +1,13 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['admin_login'])){
-        header("location: ../dangnhap.php?error= Đăng nhập trang admin thất bại");
-    }
+    include './menu_footer/menu.php';
 ?>
+            <!-- body -->
+            <div class="container">
+                <div class="body mt-3">
+                   
+                </div>
+            </div>
+            <!-- kt body -->
 <?php
-    include '../partials/header.php';
-?>
-    trang chủ admin
-    <a href="../dangxuat.php">dang xuat</a>
-<?php
-    include '../partials/bottom.php';
-?>
+    include './menu_footer/footer.php';
+?>          

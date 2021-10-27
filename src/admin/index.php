61d956beb23846a900c8f1807122004cccead5d8
dao98765 <daont1810@gmail.com>
2021-10-27 21:19:13 +0700

    them vao giao dien admin

--- src/admin/index.php
+++ src/admin/index.php
@@ -4,7 +4,35 @@
             <!-- body -->
             <div class="container">
                 <div class="body mt-3">
-                   
+                    <table class="table table-bordered ">   
+                        <thead>
+                            <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Tên người dùng</th>
+                            <th scope="col">Danh sách tài khoản</th>
+                            <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <!-- kt body -->

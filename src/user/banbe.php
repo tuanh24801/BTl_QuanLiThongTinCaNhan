33fd10a21619a7e62cfaecc36192a2e9e56f05ce
phuong32123 <www.phuong932001vn@gmail.com>
2021-10-28 21:40:55 +0700

    chinh sua giao dien ban be

--- src/user/banbe.php
+++ src/user/banbe.php
@@ -14,7 +14,12 @@
                     </div>
                     <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
                         <div class="alert alert-primary text-center" role="alert">
-                            nhắn tin
+                            Nhắn tin
+                        </div>
+                    </a>
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
+                        <div class="alert alert-primary text-center" role="alert">
+                            Hủy kết bạn
                         </div>
                     </a>
                 </div>
@@ -30,7 +35,12 @@
                     </div>
                     <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
                         <div class="alert alert-primary text-center" role="alert">
-                            nhắn tin
+                            Nhắn tin
+                        </div>
+                    </a>
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
+                        <div class="alert alert-primary text-center" role="alert">
+                            Hủy kết bạn
                         </div>
                     </a>
                 </div>
@@ -49,6 +59,12 @@
                             nhắn tin
                         </div>
                     </a>
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
+                        <div class="alert alert-primary text-center" role="alert">
+                            Hủy kết bạn
+                        </div>
+                    </a>
+
                 </div>
             </div>
             <!-- kt user bạn -->
@@ -59,9 +75,14 @@
                     <div class="card-body">
                         <p class="card-text text-center"><a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/index.php?id=..">Tên người dùng</a></p>
                     </div>
-                    <a href="">
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
                         <div class="alert alert-primary text-center" role="alert">
                             nhắn tin
+                        </div>
+                    </a>
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
+                        <div class="alert alert-primary text-center" role="alert">
+                            Hủy kết bạn
                         </div>
                     </a>
                 </div>
@@ -74,9 +95,14 @@
                     <div class="card-body">
                         <p class="card-text text-center"><a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/index.php?id=..">Tên người dùng</a></p>
                     </div>
-                    <a href="">
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
                         <div class="alert alert-primary text-center" role="alert">
                             nhắn tin
+                        </div>
+                    </a>
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
+                        <div class="alert alert-primary text-center" role="alert">
+                            Hủy kết bạn
                         </div>
                     </a>
                 </div>
@@ -89,39 +115,14 @@
                     <div class="card-body">
                         <p class="card-text text-center"><a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/index.php?id=..">Tên người dùng</a></p>
                     </div>
-                    <a href="">
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
                         <div class="alert alert-primary text-center" role="alert">
                             nhắn tin
                         </div>
                     </a>
-                </div>
-            </div>
-            <!-- kt user bạn -->
-            <!-- user bạn -->
-            <div class="col-lg-3">
-                <div class="card avatar_friend mt-3">
-                    <img src="./image_user/avatar_female.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text text-center"><a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/index.php?id=..">Tên người dùng</a></p>
-                    </div>
-                    <a href="">
+                    <a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">
                         <div class="alert alert-primary text-center" role="alert">
-                            nhắn tin
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- kt user bạn -->
-            <!-- user bạn -->
-            <div class="col-lg-3">
-                <div class="card avatar_friend mt-3">
-                    <img src="./image_user/avatar_female.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text text-center"><a href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/index.php?id=..">Tên người dùng</a></p>
-                    </div>
-                    <a href="">
-                        <div class="alert alert-primary text-center" role="alert">
-                            nhắn tin
+                            Hủy kết bạn
                         </div>
                     </a>
                 </div>

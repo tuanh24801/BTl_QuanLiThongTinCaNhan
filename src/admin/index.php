6fdf86e448d48e6806c2a81e010dcbb82dc09d6b
dao98765 <daont1810@gmail.com>
2021-10-29 07:34:41 +0700

    chinh sua giao dien admin

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,137 +1,11 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-    <!--nav-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-            <!--officanvas-->
-            <button class="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                <span class="navbar-toggler-icon" data-bs-target="#offcanvasExample">
-                </span>
-            </button>
-            <!--officanvas-->
-            <a class="navbar-brand fw-bold text-uppercase me-auto" href="#">Admin</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse my-2 my-lg-0" id="navbarSupportedContent">
-                <form class="d-flex ms-auto">
-                    <input class="form-control me-2" type="search" placeholder="Search here.." aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search </button>
-                </form>
-                <ul class="navbar-nav  mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Acount
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Đăng xuất</a></li>
-                            <!-- <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!--nav-->
-    <!--offcanvas-->
-    <div class="offcanvas offcanvas-start bg-dark text-white sidebar-nav" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-
-        <div class="offcanvas-body p-0">
-            <nav class="navbar-dark">
-                <ul class="navbar-nav">
-                    <li>
-                        <div class="text-muted small fw-bold text-uppercase px-3">
-                            CORE
-                        </div>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span><i class="fas fa-tachometer-alt"></i></span>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li class="my-4">
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                        <div class="text-muted small fw-bold text-uppercase px-3">
-                            INTERFACE
-                        </div>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span><i class="fa fa-user text-light fa-lg mr-3"></i></span>
-                            <span>Danh sách người dùng</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span><i class="fa fa-table text-light fa-lg mr-3"></i></span>
-                            <span>Danh sách tài khoản</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span><i class="fas fa-rocket"></i></i></span>
-                            <span>Nhiệm vụ</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span> <i class="fas fa-user-friends"></i></span>
-                            <span>Nhóm</span>
-                        </a>
-                    </li>
-                    <li class="my-4">
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                        <div class="text-muted small fw-bold text-uppercase px-3">
-                            ADDONS
-                        </div>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span><i class="fas fa-comment-dots"></i></span>
-                            <span>Chats</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-3 active">
-                            <span><i class="fas fa-tools"></i></span>
-                            <span>Cài Đặt </span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-
-
-        </div>
-    </div>
+<?php
+    include './menu_footer/menu.php';
+?>
     <!--offcanvas-->
     <main class="mt-5 pt-3">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-12 fw-bold fs-3">Dashboard</div>
+                <div class="col-md fw-bold fs-3 text-center">Dashboard</div>
             </div>
             <div class="row">
                 <div class="col-md-12">
@@ -210,16 +84,6 @@
         </div>
     </main>
 
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-</body>
-
-</html>
+<?php
+    include './menu_footer/footer.php'
+?>
--- src/admin/menu_footer/menu.php
+++ src/admin/menu_footer/menu.php
@@ -6,37 +6,130 @@
 ?>
 <!doctype html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-        <link rel="stylesheet" href="./css/style.css">
-        <title>Trang chủ admin</title>
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <!--nav-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <!-- menu -->
-            <div class="menu row ">
-                <nav class="nav nav-pills nav-fill">
-                    <a class="nav-link" href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/index.php">Danh sách người dùng</a>
-                    <a class="nav-link" href="http://localhost/BTL_QuanLiThongTinCaNhan/src/dangxuat.php">Danh sách tài khoản</a>
-                    <a class="nav-link" href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/banbe.php">Danh sách nhiệm vụ người dùng</a>
-                    <a class="nav-link" href="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/tinnhan.php">Nhóm</a>
-                    <a class="nav-link" href="http://localhost/BTL_QuanLiThongTinCaNhan/src/dangxuat.php">Đăng xuất</a>
-                    <nav class="navbar">
-                        <div class="container-fluid">
-                            <form class="d-flex" action="http://localhost/BTL_QuanLiThongTinCaNhan/src/user/timkiem.php" method="post">
-                                <!--<input class="form-control me-2" type="search" placeholder="Tìm kiếm" aria-label="Search" name="timkiem">
-                                <button class="btn btn-outline-warning" type="submit" name = "timkiem">Search</button>-->
-                            </form>
-                        </div>
-                    </nav>
-                </nav>
+            <!--officanvas-->
+            <button class="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon" data-bs-target="#offcanvasExample">
+                </span>
+            </button>
+            <!--officanvas-->
+            <a class="navbar-brand fw-bold text-uppercase me-auto" href="#">Trang chủ Admin</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse my-2 my-lg-0" id="navbarSupportedContent">
+                <form class="d-flex ms-auto">
+                    <!-- <input class="form-control me-2" type="search" placeholder="Search here.." aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search </button> -->
+                </form>
+                <ul class="navbar-nav  mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Acount
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Đăng xuất</a></li>
+                            <!-- <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+                        </ul>
+                    </li>
+                </ul>
             </div>
-            <!-- kt menu -->
+        </div>
+    </nav>
+    <!--nav-->
+    <!--offcanvas-->
+    <div class="offcanvas offcanvas-start bg-dark text-white sidebar-nav" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+
+        <div class="offcanvas-body p-0">
+            <nav class="navbar-dark">
+                <ul class="navbar-nav">
+                    <li>
+                        <div class="text-muted small fw-bold text-uppercase px-3">
+                            CORE
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 active">
+                            <span><i class="fas fa-tachometer-alt"></i></span>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold text-uppercase px-3">
+                            INTERFACE
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 active">
+                            <span><i class="fa fa-user text-light fa-lg mr-3"></i></span>
+                            <span>Danh sách người dùng</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="dstaikhoan.php" class="nav-link px-3 active">
+                            <span><i class="fa fa-table text-light fa-lg mr-3"></i></span>
+                            <span>Danh sách tài khoản</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 active">
+                            <span><i class="fas fa-rocket"></i></i></span>
+                            <span>Nhiệm vụ</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 active">
+                            <span> <i class="fas fa-user-friends"></i></span>
+                            <span>Nhóm</span>
+                        </a>
+                    </li>
+                    <li class="">
+                        <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                        <div class="text-muted small fw-bold text-uppercase px-3">
+                            ADDONS
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 active">
+                            <span><i class="fas fa-comment-dots"></i></span>
+                            <span>Chats</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-3 active">
+                            <span><i class="fas fa-tools"></i></span>
+                            <span>Cài Đặt </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+
+        </div>
+    </div>

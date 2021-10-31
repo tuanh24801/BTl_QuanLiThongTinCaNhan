f3af8d7b0c2d8ff763db4e2bf822ff045454d93c
phuong32123 <www.phuong932001vn@gmail.com>
2021-10-31 15:14:00 +0700

    sua script

--- src/user/script/app.js
+++ src/user/script/app.js
@@ -15,16 +15,42 @@ $(document).ready(function(){
                     '<img src="http://localhost/BTL_QuanLiThongTinCaNhan/image/image_4.jpg" class="img-fluid mb-5" alt="Sample image">');
             }})}
     })
+    // kt script cho timkiem.php
 
     // script cho dang nhiemvu.php
     $('#btndangnhiemvu').click(function(){
         var tennhiemvu = $('#txttennhiemvu').val();
         var noidungnhiemvu = $('#txtnoidungnhiemvu').val();
-        $.post('http://localhost/BTL_QuanLiThongTinCaNhan/src/user/xulynhiemvu.php', {data_1 : tennhiemvu, 
-                                                                                      data_2: noidungnhiemvu}, function(data){
+        $.post('http://localhost/BTL_QuanLiThongTinCaNhan/src/user/xulynhiemvu.php', 
+            {data_1 : tennhiemvu, data_2: noidungnhiemvu}, 
+            function(data){
+                location.reload();
 
         })
 
     })
+    // kt script cho dang nhiemvu.php
+
+
+    //script cho chỉnh sửa thông tin người dùng
+    $('#chinhsua').click(function(){
+        var tennguoidung = $('#tennguoidung').val();
+        var gioitinh = $('#gioitinh').val();
+        var ngaysinh = $('#ngaysinh').val();
+        var diachi = $('#diachi').val();
+        var sodienthoai = $('#sodienthoai').val();
+        var email = $('#email').val();
+        $.post('http://localhost/BTL_QuanLiThongTinCaNhan/src/user/xulychinhsuathongtin.php',
+        {tennguoidung : tennguoidung,gioitinh : gioitinh,ngaysinh: ngaysinh,diachi : diachi,sodienthoai: sodienthoai,email : email},
+         function(data){
+            swal("Chỉnh sửa thành công",{
+                icon:"success",
+            })
+         }
+        
+        )
+    })
+
+    //kt Script cho chỉnh sửa thông tin người dùng
 
 })
